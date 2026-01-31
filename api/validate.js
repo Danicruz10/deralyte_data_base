791e2bc1c802bdab9c0757022c43ea2f8feb8c3b
Daniel Cruz <danielfernandocruzpetoccini10@gmail.com>
2026-01-31 01:29:05 -0400

    cambios22

--- api/validate.js
+++ api/validate.js
@@ -10,7 +10,7 @@ export default async function handler(req, res) {
   res.setHeader("Access-Control-Allow-Credentials", true);
   res.setHeader(
     "Access-Control-Allow-Origin",
-    "https://wisteria-bulldog-nrwn.squarespace.com/",
+    "https://wisteria-bulldog-nrwn.squarespace.com",
   );
   res.setHeader(
     "Access-Control-Allow-Methods",
